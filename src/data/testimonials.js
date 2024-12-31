49271b4ca1a55d91a3e96f09f7dcac423d056771
Karim <kamel.benanaya001@gmail.com>
2024-12-31 23:03:23 +0100

    commit my work

---
+++ src/data/testimonials.js
@@ -0,0 +1,36 @@
+import profile1 from "../assets/image/profile-1.svg";
+import profile2 from "../assets/image/profile-2.svg";
+import profile3 from "../assets/image/profile-3.svg";
+
+const testimonialData = [
+  {
+    id: 1,
+    rating: 5,
+    img: profile1,
+    testimonial:
+      "dem ultrices at dui adipiscing aliquam faucibus. Venenatis tortor fringilla cursus", 
+    user: "Fred Tester",
+    profession: 'Actor'
+  },
+  {
+    id: 2,
+    rating: 4,
+    img: profile2,
+    testimonial:
+      "dem ultrices at dui adipiscing aliquam faucibus. Venenatis tortor fringilla cursus",
+    user: "Seymore Grillz",
+    profession: 'Graphic designer'
+  },
+  {
+    id: 3,
+    rating: 5,
+    img: profile3,
+    testimonial:
+      "dem ultrices at dui adipiscing aliquam faucibus. Venenatis tortor fringilla cursus",
+    user: "Chris Baughman",
+    profession: 'Developer'
+  },
+];
+
+
+export default testimonialData
---
+++ src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import MenuList from "../components/MenuList";
+import TestimonialList from "../components/TestimonialList";
+
+function Homepage() {
+  return (
+    <>
+      <Header />
+      <MenuList />
+      <TestimonialList />
+    </>
+  );
+}
+
+export default Homepage;
